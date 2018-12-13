f1de4a46744e705e5c8ade8209ed69378549f3a0
Reinerok <reinerok@gmail.com>
2018-12-14 01:24:56 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let pieceOfPuzzle = document.querySelectorAll('.puzzle_piece'),
+    randArrNumber = [];
+    
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+}
+
+function randArr() {
+    randArrNumber = [];
+    randArrNumber.push(randomInteger(1,8));
+    function fillArr() {
+        let test = randomInteger(1,8);
+        if (randArrNumber.indexOf(test) === -1) {
+            randArrNumber.push(test);
+                if (randArrNumber.length === 8) {
+                    return randArrNumber;
+                }
+        }
+        fillArr();
+    }
+    fillArr();
+    return randArrNumber;
+}
+
+function shufle() {
+    randArr();
+    pieceOfPuzzle.forEach((arr, i) => {
+        arr.style.order = randArrNumber[i];
+    });
+}
+shufle();
+
